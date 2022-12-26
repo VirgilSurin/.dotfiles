88b1338b71a0f9f29369eca53f415a835d40d97d
Virgil Surin <virgil.surin@student.umons.ac.be>
2022-12-26 10:40:14 +0100

    emacs

---
+++ .emacs.d/custom.el
@@ -0,0 +1,35 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(avy-all-windows 'all-frames)
+ '(comment-style 'extra-line)
+ '(custom-safe-themes
+   '("f4dd37f1767c4cce723b8f329eba5f54838861ed1b466dccc72032cd7ccf6e28" "4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" "b73a23e836b3122637563ad37ae8c7533121c2ac2c8f7c87b381dd7322714cd0" "3319c893ff355a88b86ef630a74fad7f1211f006d54ce451aab91d35d018158f" "ec4c67dda391047a6efc142a8a9e6ba5c95384a87d22e178ff304312b9cd59d4" default))
+ '(dired-listing-switches "--almost-all -l")
+ '(doom-Iosvkem-brighter-comments nil t)
+ '(doom-Iosvkem-brighter-modeline t t)
+ '(doom-dracula-brighter-comments nil)
+ '(doom-dracula-brighter-modeline t)
+ '(doom-dracula-colorful-headers t)
+ '(goal-column nil)
+ '(kill-ring-max 5)
+ '(lsp-ui-sideline-diagnostic-max-line-length 20)
+ '(lsp-ui-sideline-diagnostic-max-lines 5)
+ '(mini-frame-show-parameters '((top . 0.35) (width . 0.6) (left . 0.5)))
+ '(package-selected-packages
+   '(exwm flycheck-rust rustic dirvish hungry-delete comment-dwim-2 browse-kill-ring avy-zap smex tsc auctex pdf-tools vterm evil ein 0A11-2-low-theme 0A11-3-theme 0A11-2-theme 0A11-theme telephone-line esup smartparens ace-popup-menu autopair auto-pair treemacs-all-the-icons crux dired-x persp-mode lsp-pyright page-break-lines dashboard makefile-executor tree-sitter-langs tree-sitter magonyx-theme buffer-move visual-fill-column org-bullets groovy-mode ess racket-mode gradle-mode lsp-java elpy lsp-ui lsp-treemacs lsp-ivy lsp-mode magit java-snippets yasnippet-snippets yasnippet company flycheck iedit quickrun docstr fixmee rainbow-delimiters treemacs-icons-dired treemacs counsel-projectile projectile avy use-package-chords god-mode which-key helpful counsel ivy-hydra ivy-rich ivy mwim general dimmer solaire-mode highlight-indent-guides mini-frame doom-modeline use-package))
+ '(pos-tip-background-color "#16211C")
+ '(pos-tip-foreground-color "#dcded9")
+ '(warning-suppress-types '((comp) (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(hl-line ((t nil)))
+ '(hl-line-face ((t (:background (\, (doom-color 'violet))))))
+ '(mode-line ((t nil)))
+ '(popup-isearch-match ((t (:inherit default))))
+ '(popup-menu-selection-face ((t (:inherit fringe)))))
---
+++ .emacs.d/init.el
@@ -0,0 +1,4 @@
+(setq V_PATH "~/Documents/emacs/V-emacs/")
+
+(add-to-list 'load-path V_PATH)
+(require 'v-init)
