797e951938e7cd079e61f65912cd2f38880447d5
VirgilSurin <virgil.surin@student.umons.ac.be>
2023-11-05 13:32:27 +0100

    lock screen blur

--- lock_screen.sh
+++ lock_screen.sh
@@ -1,6 +1,35 @@
 #!/usr/bin/env sh
 
-tmpbg='/tmp/screen.png'
-scrot $tmpbg -o
-
-i3lock-color -i "$tmpbg" -eu -p "win" --wrong-text="Ah ah ah! You didn't say the magic word!"
+BLANK='#00000000'
+CLEAR='#dfdfdf'
+DEFAULT='#dfdfdf'
+TEXT='#dfdfdf'
+WRONG='#ff6c6b'
+VERIFYING='#c678dd'
+# i3lock-color -i "$tmpbg" -eu -p "win" --wrong-text="Ah ah ah! You didn't say the magic word!"
+i3lock-color \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
+\
+--screen 1                   \
+--blur 5                     \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M:%S"        \
+--date-str="%d/%m/%Y"       \
