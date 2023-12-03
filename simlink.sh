bdfd8e61aea0396af3e7173ba9fcf7f6de75710e
VirgilSurin <virgil.surin@student.umons.ac.be>
2023-12-03 17:42:51 +0100

    fixed simlink

--- simlink.sh
+++ simlink.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env sh
 # sudo !
 
-ln -sf config.py ~/.config/qtile/config.py
-ln -sf theme.py ~/.config/qtile/theme.py
+rm -f ~/.config/qtile/config.py
+rm -f ~/.config/qtile/themes.py
+sudo rm -f //etc/nixos/home-configuration.nix
+sudo rm -f //etc/nixos/configuration.nix
+sudo rm -f //etc/nixos/sddm-theme.nix
 
-sudo ln -f home.nix //etc/nixos/home-configuration.nix
-sudo ln -f configuration.nix //etc/nixos/configuration.nix
-sudo ln -f sddm-theme.nix //etc/nixos/sddm-theme.nix
+ln -f ./config.py ~/.config/qtile/config.py
+ln -f ./themes.py ~/.config/qtile/themes.py
+sudo ln -f ./home.nix //etc/nixos/home-configuration.nix
+sudo ln -f ./configuration.nix //etc/nixos/configuration.nix
+sudo ln -f ./sddm-theme.nix //etc/nixos/sddm-theme.nix
