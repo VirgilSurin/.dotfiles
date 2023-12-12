1d113200c2c21a97ae36f742afe78913977e148a
VirgilSurin <virgil.surin@student.umons.ac.be>
2023-12-12 22:16:29 +0100

    script to generate a shell.nix for python

---
+++ make-shell.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+echo "{ pkgs ? import <nixpkgs> {} }:
+
+let
+  python-packages = ps: with ps; [
+
+  ];
+  my-python = pkgs.python311.withPackages python-packages;
+in
+pkgs.mkShell {
+  packages = [
+    (my-python)
+  ];
+}" >> shell.nix
+
+echo "use nix" >> .envrc
+
+direnv allow
