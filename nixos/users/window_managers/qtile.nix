fb98d19a0380dbd7215290f544ac64e59880a2b6
Virgil Surin <virgil.surin@student.umons.ac.be>
2025-02-05 16:41:28 +0100

    qtile.nix

--- nixos/users/window_managers/qtile.nix
+++ nixos/users/window_managers/qtile.nix
@@ -2,4 +2,5 @@
 
 {
   home.file.".config/qtile/config.py".source = ./users/window_managers/qtile/config.py;
+  home.file.".config/qtile/themes.py".source = ./users/window_managers/qtile/themes.py;
 }
