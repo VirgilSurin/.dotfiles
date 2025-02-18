ec6f36fb139472b2fe6901d40ab9df1647b85ec9
Virgil Surin <virgil.surin@student.umons.ac.be>
2025-02-18 14:29:36 +0100

    rofi sound

--- nixos/users/pkgs/rofi-sound/default.nix
+++ nixos/users/pkgs/rofi-sound/default.nix
@@ -1,7 +1,8 @@
 { lib
 , stdenv
 , makeWrapper
-, pulseaudio
+, pipewire
+, rofi
 , dunst
 , fetchFromGitHub
 }:
@@ -10,28 +11,25 @@ stdenv.mkDerivation {
   pname = "rofi-sound";
   version = "1.0.0";
 
-  src = fetchFromGitHub {
-    owner = "kellya";
-    repo = "rofi-sound";
-    rev = "b32e85ecc81c3b6df1c3a88187210cbf8cabb513";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
-  };
+  src = ./.;
+
+  nativeBuildInputs = [ makeWrapper ];
 
   dontBuild = true;
 
   installPhase = ''
-    install -Dm755 rofi-sound $out/bin/rofi-sound
+    install -Dm755 rofi-sound-output-chooser.sh $out/bin/rofi-sound
     wrapProgram $out/bin/rofi-sound \
       --prefix PATH : ${lib.makeBinPath [
-        pulseaudio
+        pipewire
+        rofi
         dunst
       ]}
   '';
 
   meta = with lib; {
-    description = "Script using rofi to allow sound output selection";
-    homepage = "https://github.com/kellya/rofi-sound";
-    license = license.mit;
+    description = "Script using rofi to allow sound output selection with PipeWire";
+    license = licenses.mit;
     platforms = platforms.linux;
     mainProgram = "rofi-sound";
   };
