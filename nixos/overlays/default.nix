451f6841b4b0f88f23ae8737ddbaa0df13acfcef
Virgil Surin <virgil.surin@student.umons.ac.be>
2025-06-04 15:39:33 +0200

    [nixos] qtile 0.29.0 -> 0.31.0

--- nixos/overlays/default.nix
+++ nixos/overlays/default.nix
@@ -1,36 +1,26 @@
 { inputs, ... }:
 
 {
+  # imports my custom pkgs from the "pkgs" directory
+  # additions = final: _prev: import ../pkgs { pkgs = final; };
+
   unstable-packages = final: _prev: {
-
-    # imports my custom pkgs from the "pkgs" directory
-    additions = final: _prev: import ../pkgs final.pkgs;
-
-    # accessble through "pkgs.unstable"
+    # accessible through "pkgs.unstable"
     unstable = import inputs.unstable {
       system = final.system;
       config.allowUnfree = true;
     };
+  };
 
-    # for custom modifications to packages (version changes etc)
-    modifications = final: prev: {
-      hello = prev.hello.overrideAttrs (oldAttrs: rec {
-        version = "2.10";
-        src = prev.fetchurl {
-          url = "https://ftp.gnu.org/gnu/hello/hello-${version}.tar.gz";
-          sha256 = "";
-        };
-      });
-
-      qtile = prev.qtile-unwrapped.overrideAttrs (oldAttrs: rec {
-        version = "0.31.0";
-        src = prev.fetchFromGitHub {
-          owner = "qtile";
-          repo = "qtile";
-          tag = "v${version}";
-          hash = "sha256-EqrvBXigMjevPERTcz3EXSRaZP2xSEsOxjuiJ/5QOz0=";
-        };
-      });
-    };
+  modifications = final: prev: {
+    qtile= prev.qtile.overrideAttrs (oldAttrs: rec {
+      version = "0.31.0";
+      src = prev.fetchFromGitHub {
+        owner = "qtile";
+        repo = "qtile";
+        tag = "v${version}";
+        hash = "sha256-EqrvBXigMjevPERTcz3EXSRaZP2xSEsOxjuiJ/5QOz0=";
+      };
+    });
   };
 }
