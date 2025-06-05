11a72de3f9b800a9081d2d9b926da6f4ae18d3e7
Virgil Surin <virgil.surin@student.umons.ac.be>
2025-06-05 15:28:47 +0200

    [nixos] sudo -> doas WIP

--- nixos/users/virgil/configuration.nix
+++ nixos/users/virgil/configuration.nix
@@ -12,32 +12,22 @@
   ];
 
   colorScheme = inputs.nix-colors.colorSchemes.onedark;
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true; 
+
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Setup keyfile
   boot.initrd.secrets = { "/crypto_keyfile.bin" = null;
   };
 
   boot.supportedFilesystems = [ "ntfs" ];
 
   networking.hostName = "VS-chimaera";
-
-  # Configure network proxy if necessary networking.proxy.default }= 
-  # "http://user:password@proxy:port/"; networking.proxy.noProxy = 
-  # "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
   systemd.services.NetworkManager-wait-online.enable = pkgs.lib.mkForce false;
 
-  # Set your time zone.
   time.timeZone = "Europe/Brussels";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "fr_BE.UTF-8";
     LC_IDENTIFICATION = "fr_BE.UTF-8";
@@ -55,8 +45,6 @@
   users.extraGroups.vboxusers.members = [ "virgil" ];
 
   services.hardware.bolt.enable = true;
-
-  # Configure keymap in X11
 
   services = {
     libinput.enable = true;
@@ -119,13 +107,7 @@
       enable = true;
     };
   };
-  # Lock
-  # programs.xss-lock = {
-  #   enable = true;
-  #   lockerCommand = "${pkgs.sddm}/bin/sddm";
-  # };
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
   services.power-profiles-daemon.enable = false;
@@ -148,24 +130,17 @@
     };
   };
 
-  # Enable sound with pipewire.
   services.pulseaudio.enable = true;
   nixpkgs.config.pulseaudio = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = false;
-    # alsa.enable = true;
-    # alsa.support32Bit = true;
-    # pulse.enable = true;
-    # # If you want to use JACK applications, uncomment this
     # jack.enable = true;
   };
 
-  # usb
   services.gvfs.enable = true;
   services.udisks2.enable = true;
 
-  # fingerprint auth !
   services.fprintd = {
     enable = false;
     tod.enable = true;
@@ -196,17 +171,12 @@
   backupFileExtension = "backup-$(date +%Y%m%d)";
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "jetbrains-toolbox"
-    "jetbrains.idea-ultimate"
-    "jetbrains.pycharm-community"
     "discord"
-    "slack"
     "keymapp"
   ];
-  # nixpkgs.config.allowUnfreePredicate = _: true;
+
   system.autoUpgrade.enable = true;
   system.autoUpgrade.dates = "weekly";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -217,11 +187,7 @@
     options = "--delete-older-than 10d";
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    bolt
     discord
     slack
     wget
@@ -231,9 +197,7 @@
     home-manager
     keymapp
     spotify
-    xorg.xinit
 
-    hello
     libdrm
     mesa
     wayland
@@ -247,29 +211,6 @@
     usbutils
     udiskie
     udisks
-
-    (where-is-my-sddm-theme.override {
-      themeConfig.General = {
-       passwordCharacter = "*";
-       passwodInputBackground = "#${config.colorScheme.palette.base01}";
-       passwodCursorColor = "#${config.colorScheme.palette.base05}";
-       passwodTextColor = "#${config.colorScheme.palette.base05}";
-
-       showSessionBuDefault = true;
-
-       backgroundFill = "#${config.colorScheme.palette.base00}";
-       basicTextColor = "#${config.colorScheme.palette.base05}";
-      };
-    })
-
-    # Hyprland utilities
-    hyprpaper
-    hyprpicker
-    xwayland
-    qt6.qtwayland
-    grim
-    slurp
-    wl-clipboard
 
     # qt5
     libsForQt5.qt5.qtquickcontrols2
@@ -290,8 +231,8 @@
   };
 
   networking.stevenBlackHosts = {
-    blockFakenews = false;
-    blockGambling = false;
+    blockFakenews = true;
+    blockGambling = true;
     blockPorn = true;
     blockSocial = false;
   };
@@ -300,6 +241,17 @@
     vial
     via
   ];
+  hardware.keyboard.zsa.enable = true;
+
+  security.sudo.enable = true;
+  security.doas = {
+    enable = true;
+    extraRules = [{
+      users = [ "virgil" ];
+      keepEnv = true;
+      persist = true;
+    }];
+  };
 
   security.pam.services = {
     i3lock = {
@@ -317,33 +269,6 @@
   services.blueman.enable = true;
 
 
-  # I now use a ZSA keyboard, I must enable some udev rules for it
-  hardware.keyboard.zsa.enable = true;
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "23.05"; # DO NOT CHANGE
 
 }
