985c451ba87e3599bc7db4ba77e78ad9d9946338
VirgilSurin <virgil.surin@protonmail.com>
2025-07-18 23:39:36 +0200

    [nixos] typo fix

--- nixos/home/configuration.nix
+++ nixos/home/configuration.nix
@@ -4,7 +4,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../modules.common_config.nix
+      ../modules/common_config.nix
       inputs.home-manager.nixosModules.default
     ];
 
