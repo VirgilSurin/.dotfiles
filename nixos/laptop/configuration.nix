d0f4e76b84e9e384790bae0c7609b7615d946004
Virgil Surin <virgil.surin@student.umons.ac.be>
2025-08-04 12:20:24 +0200

    [nixos] tailscale on Chimaera

--- nixos/laptop/configuration.nix
+++ nixos/laptop/configuration.nix
@@ -7,7 +7,6 @@
     ./hardware-configuration.nix
     ../modules/common_config.nix
     inputs.home-manager.nixosModules.default
-    inputs.stylix.nixosModules.stylix
   ];
 
   stylix = {
@@ -190,6 +189,8 @@
     enable = true;
     package = pkgs.emacs-gtk;
   };
+
+  services.tailscale.enable = true;
 
   networking.stevenBlackHosts = {
     blockFakenews = true;
